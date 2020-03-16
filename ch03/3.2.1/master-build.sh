bc985cf9e9b63b193a85d3a0a07c173121a4abb4
inforix <yupingwang@gmail.com>
2020-03-16 18:08:57 +0800

    Update master-build.sh

--- ch03/3.2.1/master-build.sh
+++ ch03/3.2.1/master-build.sh
@@ -1,3 +1,7 @@
+# 这种方法已经逐渐被淘汰了
 kubeadm init   --kubernetes-version v1.14.0 \
                --pod-network-cidr 10.244.0.0/16 \
                --image-repository registry.aliyuncs.com/google_containers
+
+# 2. 建议使用该方法
+kubeadm init --config kubeconfig.yaml
