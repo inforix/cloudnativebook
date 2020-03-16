4f09642d98fa6da639cb7bef7fbb1e8583766977
inforix <yupingwang@gmail.com>
2020-03-16 17:41:20 +0800

    Update prepare.sh

--- ch03/3.2.1/prepare.sh
+++ ch03/3.2.1/prepare.sh
@@ -107,6 +107,9 @@ yum install -y bash-completion
 source /usr/share/bash-completion/bash_completion
 source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo "alias k=kubectl" >> ~/.bashrc
+echo "complete -F __start_kubectl k" >> ~/.bashrc
+
 
 
 
